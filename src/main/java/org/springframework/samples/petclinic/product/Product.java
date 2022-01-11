03e894ad21106134a8d027540c2e095cd675a687
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-01-11 14:13:40 +0100

    probando simulacro

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,34 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
+    @Id
     Integer id;
+
+    @NotNull
+    @Size(min=3,max=50)
     String name;
+
+    @Min(value=0)
     double price;
+    
+    @ManyToOne
     ProductType productType;
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -1,11 +1,16 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class ProductType {
+    @Id
     Integer id;
     String name;
 }
